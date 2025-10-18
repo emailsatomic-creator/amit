3c6b3370028be073a5218117fdc15ee83b8ffad3
emailsatomic-creator <emailsatomic@gmail.com>
2025-10-18 17:13:28 +0530

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,26 @@
+import { Navigation } from "@/components/Navigation";
+import { Hero } from "@/components/Hero";
+import { About } from "@/components/About";
+import { RecipeSection } from "@/components/RecipeSection";
+import { Testimonials } from "@/components/Testimonials";
+import { Footer } from "@/components/Footer";
+import { ParticleBackground } from "@/components/ParticleBackground";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background relative overflow-hidden">
+      <ParticleBackground />
+      
+      <div className="relative z-10">
+        <Navigation />
+        <Hero />
+        <About />
+        <RecipeSection />
+        <Testimonials />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Index;
